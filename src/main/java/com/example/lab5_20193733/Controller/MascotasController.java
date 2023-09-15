ae0893447d3f7e6c188aefa826e66e210ff24e05
Jose Morillos <jmorillosp@pucp.edu.pe>
2023-09-14 21:56:34 -0500

    Nuevos cambios

--- src/main/java/com/example/lab5_20193733/Controller/MascotasController.java
+++ src/main/java/com/example/lab5_20193733/Controller/MascotasController.java
@@ -5,7 +5,6 @@ import com.example.lab5_20193733.Entity.Mascotas;
 import com.example.lab5_20193733.Repository.MascotasRepository;
 import com.example.lab5_20193733.Repository.PersonaRepository;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
